2946bc3af93ed83e8cb8a648810214f2abd4f9c0
John Kamenik <jkamenik@gmail.com>
2011-07-15 20:19:36 -0400

    Basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodDiary::Application.config.secret_token = 'c9e7d4deeed83ece81840f9c83390d787cf03a78315eedfc923d211a2033828a53f3ad39c48a82d8b77ed0904f80e7086598de42226a6a854994a34f45647059'
