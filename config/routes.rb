0e406ba1d252caca12dd689c21619350bfca19ca
John Kamenik <jkamenik@gmail.com>
2011-07-15 22:56:28 -0400

    Entries is now the index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 FoodDiary::Application.routes.draw do
   resources :entries
+  root :to => 'entries#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
