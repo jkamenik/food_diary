70dd27a87e301ff8afbebdfd5ef5ba6baef223ad
John Kamenik <jkamenik@gmail.com>
2011-07-18 11:50:33 -0400

    Adding the validations

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,8 +1,32 @@
 class Entry < ActiveRecord::Base
-  before_create :set_time
+  before_validation :set_time
+  before_validation :copy_older
+  
+  validates_presence_of :food
+  validates_presence_of :amount
+  
+  validates_format_of :meal_time, :with => /\d?\d:\d\d(am|pm)/, :message => 'should be in the form of a time'
+  validates_presence_of :location
+  validates_presence_of :sensory
+  validates_presence_of :emotional
   
 private
   def set_time
     self.time ||= Time.now.to_date
+  end
+  
+  def copy_older
+    # time is set eariler
+    older = Entry.find_last_by_time self.time
+    return unless older
+    
+    puts self.inspect
+    
+    self.meal_time = older.meal_time if self.meal_time.blank?
+    self.location  = older.location if self.location.blank?
+    self.sensory   = older.sensory if self.sensory.blank?
+    self.emotional = older.emotional if self.emotional.blank?
+    
+    puts self.inspect
   end
 end
